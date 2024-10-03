894010b4f40809804a843976ca461c8f9b76ea50
Peter Armington <peter.armington@gmail.com>
2024-10-03 12:58:27 -0400

    start seed db project

---
+++ SeedAirports/Program.cs
@@ -0,0 +1,10 @@
+﻿using FlightSystem.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine(File.Exists("../localflights.dat"));
+var contextOptions = new DbContextOptionsBuilder<FlightContext>();
+contextOptions.UseSqlite("Data Source=../localflights.dat");
+var context = new FlightContext(contextOptions.Options);
+context.Database.EnsureCreated();
+Console.WriteLine(context.Airports.Count());
