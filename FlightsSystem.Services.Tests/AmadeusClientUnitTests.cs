fb32b0d22eb55c172e9f43f7af3098facc1acb63
Peter Armington <peter.armington@gmail.com>
2024-11-18 09:04:26 -0500

    add unit test

--- FlightsSystem.Services.Tests/AmadeusClientUnitTests.cs
+++ FlightsSystem.Services.Tests/AmadeusClientUnitTests.cs
@@ -6,24 +6,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using Moq;
+using Moq.Protected;
+using System.Net.Http.Json;
+using static FlightSystem.Services.AmadeusClient;
 
 namespace FlightsSystem.Services.Tests;
 
 [TestClass]
 public class AmadeusClientUnitTests
 {
-
     [TestMethod]
     public async Task SearchFlights()
     {
-        using var serviceProvider = TestHelpers.BuildServiceCollection().BuildServiceProvider();
+        var services = TestHelpers.BuildServiceCollection();
+
+        var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+        handlerMock
+           .Protected()
+           // Setup the PROTECTED method to mock
+           .Setup<Task<HttpResponseMessage>>(
+              "SendAsync",
+              ItExpr.IsAny<HttpRequestMessage>(),
+              ItExpr.IsAny<CancellationToken>()
+           )
+           // prepare the expected response of the mocked http call
+           .ReturnsAsync(new HttpResponseMessage()
+           {
+               StatusCode = HttpStatusCode.OK,
+               Content = JsonContent.Create(new FlightOffersResponse())
+           })
+           .Verifiable();
+
+        // use real http client with mocked handler here
+        var httpClient = new HttpClient(handlerMock.Object)
+        {
+            BaseAddress = new Uri("http://test.com/"),
+        };
+        services.AddHttpClient<AmadeusClient>((serviceProvider, client) =>
+        {
+            client.BaseAddress = new Uri("https://test.api.amadeus.com/");
+        });
+
+        using var serviceProvider = services.BuildServiceProvider();
         using var scope = serviceProvider.CreateScope();
-        var client = serviceProvider.GetRequiredService<AmadeusClient>();
+        var amadeusClient = serviceProvider.GetRequiredService<AmadeusClient>();
 
         var origin = new IataCode("EWR");
         var destination = new IataCode("SLC");
 
-        var flights = await client.SearchFlightsAsync(origin, destination, DateTime.Now);
+        var flights = await amadeusClient.SearchFlightsAsync(origin, destination, DateTime.Now);
 
         Assert.IsTrue(flights.Any());
     }
