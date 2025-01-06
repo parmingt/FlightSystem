c12a77a1f19a4ec03ac41e33467b048eea1adf3f
Peter Armington <peter.armington@gmail.com>
2025-01-06 08:58:39 -0500

    add cache and confirm flight method

--- Services/FlightSearchService.cs
+++ Services/FlightSearchService.cs
@@ -1,23 +1,41 @@
 ﻿using AmadeusSDK;
+using AmadeusSDK.Models;
 using FlightSystem.Data;
 using FlightSystem.Services.Models;
+using Microsoft.Extensions.Caching.Memory;
 using static AmadeusSDK.AmadeusClient;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace FlightSystem.Services;
 
 public class FlightSearchService
 {
     private readonly AmadeusClient routesClient;
+    private readonly IMemoryCache memoryCache;
 
-    public FlightSearchService(AmadeusClient routesClient)
+    public FlightSearchService(AmadeusClient routesClient, IMemoryCache memoryCache)
     {
         this.routesClient = routesClient;
+        this.memoryCache = memoryCache;
     }
 
     public async Task<List<Models.Flight>> SearchFlightsAsync(IataCode origin, IataCode destination, DateOnly travelDate)
     {
         var offers = await routesClient.SearchFlightsAsync(origin.ToString(), destination.ToString(), travelDate.ToDateTime(TimeOnly.MinValue));
-        var flights = offers.Select(o => o.ToFlight()).ToList();
+        var flights = offers.Select(o => {
+            var flight = o.ToFlight();
+            memoryCache.Set(("offers", flight), o);
+            return flight;
+        }).ToList();
         return flights;
+    }
+
+    public async Task<bool> ConfirmFlight(Flight flight)
+    {
+        if (!memoryCache.TryGetValue(("offers", flight), out OffersSearch.Offers cachedOffer))
+            return false;
+
+        var confirmation = await routesClient.ConfirmFlightOffer(new List<OffersSearch.Offers> { cachedOffer });
+        return confirmation.First().price.total == cachedOffer.price.total;
     }
 }
