075a0b8f4bc3b8a6e1602e8d12bb85398554b887
Peter Armington <peter.armington@gmail.com>
2025-08-26 09:12:48 -0400

    initial booking processor

---
+++ FlightSystem.BookingListener/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+
+Console.WriteLine("Hello, World!");
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:19092",
+    GroupId = "booking-engine",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+{
+    consumer.Subscribe("flight-orders");
+    while (true)
+    {
+        var consumeResult = consumer.Consume();
+        Console.WriteLine($"Received message at {consumeResult.TopicPartitionOffset}: {consumeResult.Message.Value}");
+        // Simulate processing the booking
+        Console.WriteLine("Processing booking...");
+        Thread.Sleep(2000); // Simulate some work
+        Console.WriteLine("Booking processed.");
+    }
+}
