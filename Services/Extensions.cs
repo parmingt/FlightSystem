906f5f58b2c762bc14fac55027b3f440a937ac3f
Peter Armington <peter.armington@gmail.com>
2025-09-30 09:45:07 -0400

    work on Offer dto

--- Services/Extensions.cs
+++ Services/Extensions.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static AmadeusSDK.Models.OffersSearch;
+using Price = FlightSystem.Services.Models.Price;
+using Segment = FlightSystem.Services.Models.Segment;
 
 namespace FlightSystem.Services;
 
@@ -22,5 +24,50 @@ internal static class Extensions
                 , new IataCode(s.arrival.iataCode)
                 , s.departure.at)).ToList()
             , offer.id);
+    }
+
+    internal static Offers FromOffer(this Models.Offer offer)
+    {
+        return new Offers
+        {
+            price = new OffersSearch.Price
+            {
+                total = offer.Price.Total.ToString(),
+                currency = offer.Price.Currency
+            },
+            itineraries = [
+                new Itinerary
+                {
+                    segments = offer.Segments.Select(s => new OffersSearch.Segment
+                    {
+                        carrierCode = s.CarrierCode,
+                        number = s.Number,
+                        departure = new Departure
+                        {
+                            iataCode = s.Origin.ToString(),
+                            at = s.Departure
+                        },
+                        arrival = new Arrival
+                        {
+                            iataCode = s.Destination.ToString(),
+                            at = s.Departure.AddHours(2) // Assuming a fixed duration for simplicity
+                        }
+                    }).ToArray()
+                }
+            ]
+        };
+    }
+
+    internal static Offer ToOffer(this OffersSearch.Offers offer)
+    {
+        return new Offer(
+            new Price(Decimal.Parse(offer.price.total), offer.price.currency),
+            offer.itineraries[0].segments.ToList().Select(s =>
+                new Segment(s.carrierCode
+                , s.number
+                , new IataCode(s.departure.iataCode)
+                , new IataCode(s.arrival.iataCode)
+                , s.departure.at)).ToList()
+            );
     }
 }
---
+++ Services/Models/Offer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSystem.Services.Models;
+
+public record Offer(Price Price, List<Segment> Segments)
+{
+}
